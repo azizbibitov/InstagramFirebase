fbd34744b8cb7a642f9451626a8aa4c4b22d3470
Aziz Bibitov <azizbibitov11@gmail.com>
2023-09-15 13:06:19 +0500

    Tutorial 22

---
+++ InstagramFirebasePikir/Utils/FirebaseUtils.swift
@@ -0,0 +1,24 @@
+//
+//  FirebaseUtils.swift
+//  InstagramFirebasePikir
+//
+//  Created by Aziz Bibitov on 15.09.2023.
+//
+
+import Foundation
+import FirebaseDatabase
+
+extension Database {
+    
+    static func fetchUserWithUID(uid: String, completion: @escaping (User) -> ()) {
+        Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            guard let userDictionary = snapshot.value as? [String: Any] else { return }
+            let user = User(uid: uid, dictionary: userDictionary)
+            completion(user)
+            
+        }) { (err) in
+            print("Failed to fetch user for posts:", err)
+        }
+    }
+}
